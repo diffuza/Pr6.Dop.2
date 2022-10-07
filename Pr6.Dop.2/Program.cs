14e392f782b3f06885dd14e0b9186eafb21b32f3
USER <USER@DESKTOP-TRQG01D>
2022-10-07 17:33:42 +0300

    Добавьте файлы проекта.

---
+++ Pr6.Dop.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result; //присвоение лок.переменной "result" целочисленного типа "long".
+            const long km = 149800000; // Присвоение целочисленного типа "long" лок.переменной "km" и присвоение значения константы.
+            result = km * 1000 * 100; // Вычисление лок.переменной "result"
+            Console.WriteLine(result);// вывод на экран результата вычисления "result".
+        }
+    }
+}
